41e49b3c23c7cce0c0ed74ad277a6c197a1c52b2
ozziexyz <ozzieuh@gmail.com>
2026-08-18 14:44:22 -0400

    feat: added base plane world SDF

---
+++ warehouse_sim/launch/sim.launch.py
@@ -0,0 +1,21 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    pkg_warehouse_sim = get_package_share_directory('warehouse_sim')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    world_path = os.path.join(pkg_warehouse_sim, 'worlds', 'warehouse.sdf')
+
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': f'-r {world_path}'}.items(),
+    )
+
+    return LaunchDescription([gz_sim])
