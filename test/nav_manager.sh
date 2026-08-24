b484658d9955a02e24d30dc9e7ca0a11248c5506
ozziexyz <ozzieuh@gmail.com>
2026-08-24 11:40:08 -0400

    fix: reflect new robot size in diff_drive_controller config

--- test/nav_manager.sh
+++ test/nav_manager.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ros2 topic pub --once /goal geometry_msgs/msg/PoseStamped "{header: {stamp: {sec: 0, nanosec: 0}, frame_id: 'map'}, pose: {position: {x: $1, y: $2, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}}}"
+ros2 topic pub --once /goal geometry_msgs/msg/PoseStamped "{header: {stamp: {sec: 0, nanosec: 0}, frame_id: 'odom'}, pose: {position: {x: $1, y: $2, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}}}"
