e27657f19dbb34a424bdedc77ccc9bbc83f7b022
ozziexyz <ozzieuh@gmail.com>
2026-09-11 19:31:31 -0400

    chore: remove debugging scripts

--- test/nav_manager.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ros2 action send_goal /navigate_to_pose warehouse_interfaces/action/NavigateToPose \
-  "{pose: {header: {frame_id: 'map'}, pose: {position: {x: $1, y: $1, z: 0.0}, orientation: {w: 1.0}}}}" \
-  --feedback
--- test/order_manager.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ros2 topic pub --once /order std_msgs/msg/Int32MultiArray "{data: [$1, $2, $3]}"
